9e610a51d77276457b82a662265d98d7a022d9a6
Aps136 <aparnakaranth654@gmail.com>
2025-02-09 22:24:48 +0530

    Update convert.s

--- convert.s
+++ convert.s
@@ -10,21 +10,21 @@ __Vectors
     EXPORT Reset_Handler
 Reset_Handler
 		LDR R1, =0X378
-		AND R2, R1, #0X0F
+		AND R2, R1, #0X0F; extract digits
 		MOV R3, #0
 		MOV R4, #0
 		MOV R6, #2
 		MOV R8, #0XA
 LOOP 
 		ADD R2, R3
-		LSR R1, #4
-		AND R3, R1, #0X0F
-		ADD R4, #1
-		MOV R5, #0
+		LSR R1, #4; shift to next digit
+		AND R3, R1, #0X0F; extract digit
+		ADD R4, #1; counts shifts
+		MOV R5, #0; inner loop counter
 LOOP1 
-		MUL R3, R8
-		ADD R5, #1
-		TEQ R4, R5
+		MUL R3, R8  ;multiply by 10
+		ADD R5, #1; increment inner loop counter
+		TEQ R4, R5;
 		BNE LOOP1
 		SUBS R6, #1
 		BNE LOOP
