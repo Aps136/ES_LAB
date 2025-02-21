d369742833d8c92857e62ca605940d94f7fe16d9
Aps136 <aparnakaranth654@gmail.com>
2025-02-21 10:04:13 +0530

    Create bsort.s

---
+++ lab5/bsort.s
@@ -0,0 +1,56 @@
+    AREA RESET, DATA, READONLY
+    EXPORT __Vectors
+__Vectors
+    DCD 0x10001000           ; Initial stack pointer
+    DCD Reset_Handler         ; Reset handler
+    ALIGN
+
+    AREA mycode, CODE, READONLY
+    ENTRY
+    EXPORT Reset_Handler
+
+Reset_Handler
+	LDR R0, =ARRAY
+    LDR R1, =RESULT
+    MOV R2, #10              ; Array size counter
+    MOV R3, #0               ; Offset counter    ; Copy ARRAY into RESULT
+COPY_ARRAY
+    LDR R4, [R0, R3]         ; Load element from ARRAY
+    STR R4, [R1, R3]         ; Store in RESULT
+    ADD R3, R3, #4           ; Move to next element
+    SUBS R2, R2, #1          ; Decrement count
+    BHI COPY_ARRAY           ; Repeat until done
+
+   
+    LDR R0, =RESULT          ; Load RESULT base address
+    MOV R5, #9               ; Outer loop counter (n-1)
+
+OUTERLOOP
+    MOV R7, R0               ; Reset pointer to start
+    MOV R3, R5               ; Inner loop counter
+
+INNERLOOP
+    LDR R8, [R7]             ; Load A[i]
+    LDR R9, [R7, #4]         ; Load A[i+1]
+
+    CMP R8, R9               ; Compare A[i] and A[i+1]
+    BLS NO_SWAP              ; If A[i] <= A[i+1], no swap
+
+    ; Swap A[i] and A[i+1]
+    STR R9, [R7]             ; Store A[i+1] at A[i]
+    STR R8, [R7, #4]         ; Store A[i] at A[i+1]
+
+NO_SWAP
+    ADD R7, R7, #4           ; Move to next element
+    SUBS R3, R3, #1          ; Decrement inner loop counter
+    BNE INNERLOOP            ; Repeat inner loop
+
+    SUBS R5, R5, #1          ; Decrement outer loop counter
+    BNE OUTERLOOP            ; Repeat outer loop
+
+STOP  B STOP   
+ARRAY DCD 0X10, 0X05, 0X33, 0X24, 0X56, 0X77, 0X21, 0X04, 0X87, 0X01
+          
+    AREA mydata, DATA, READWRITE
+RESULT DCD 0,0,0,0,0,0,0,0,0,0		
+		END
