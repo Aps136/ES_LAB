a21e0c507c41209363580dc4e5affd5a75e3a20b
Aps136 <aparnakaranth654@gmail.com>
2025-03-13 15:34:22 +0530

    Update ex1.c

--- lab7/ex1.c
+++ lab7/ex1.c
@@ -1,21 +1,23 @@
 #include <LPC17xx.h>
+#include <stdio.h>
+
 void delay(void); 
+unsigned char count = 0; 
+unsigned int i;
 
 int main(void) {
     SystemInit();
     SystemCoreClockUpdate();
     LPC_PINCON->PINSEL0 = 0x00000000; // Set GPIO
-    LPC_GPIO0->FIODIR = 0x00000FF0;   // Set P0.4 to P0.11 as output (8 LEDs)
-
-    unsigned char count = 0; 
-
+    LPC_GPIO0->FIODIR = 0x00000FF0;   // Set P0.4 to P0.11 as output (8 LEDs)  
     while (1) {
-        LPC_GPIO0->FIOPIN = (count << 4); // Display counter value on LEDs
-        delay(); 
-        count++;  // Increment counter (0 to 255)
+			LPC_GPIO0->FIOCLR = 0X00000FF0;
+      LPC_GPIO0->FIOSET = (count << 4); // Display counter value on LEDs
+      //delay(); 
+      count++;  // Increment counter (0 to 255)
     }
 }
 
 void delay(void) {
-    for (int i = 0; i < 1000000; i++);
+    for (i = 0; i < 1000000; i++);
 }
