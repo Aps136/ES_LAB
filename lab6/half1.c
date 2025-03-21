d7762362b23e272b63bfc0a18c7f10e1e525e048
Aps136 <aparnakaranth654@gmail.com>
2025-03-21 07:04:36 +0530

    Create half1.c

---
+++ lab6/half1.c
@@ -0,0 +1,44 @@
+#include<LPC17xx.h>
+#include <stdio.h>
+unsigned int j,i;
+unsigned long LED= 0X00000FF0;
+int main(void)
+{
+	SystemInit();
+	SystemCoreClockUpdate();	
+	LPC_PINCON->PINSEL0 = 0X00000000;
+	LPC_GPIO0->FIODIR = 0X00000FF0;	
+	while (1){
+		if((LPC_GPIO2->FIOPIN&1<<12)){			
+	  LED=0X00000010;		
+		for(i=0; i<4; i++){		
+			LPC_GPIO0->FIOSET=LED;
+			//for(j=0; j<1000000; j++);
+			LED<<=1;			
+	}	
+	LED=0X00000010;
+	for(i=0; i<4; i++){
+			
+			LPC_GPIO0->FIOCLR=LED;
+			//for(j=0; j<1000000; j++);
+			LED<<=1;
+	}
+	
+}	
+	else
+	{
+		
+		LED=0X00000080;
+		//LED<<=4;
+		for(i=0; i<9; i++){		
+			LPC_GPIO0->FIOSET=LED;
+			//for(j=0; j<1000000; j++);
+			
+			LPC_GPIO0->FIOCLR=LED;
+			//for(j=0; j<1000000; j++);
+			LED<<=1;
+	}
+	}
+	
+}
+}
