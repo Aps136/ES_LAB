e034a53413f0b20edb0d2dcf66d829e3a39663c5
Aps136 <aparnakaranth654@gmail.com>
2025-03-21 09:08:40 +0530

    Create leftoright.c

---
+++ lab6/leftoright.c
@@ -0,0 +1,26 @@
+#include <LPC17xx.h>
+#include <stdio.h>
+void delay(void); 
+unsigned char count = 0; 
+unsigned int LED = 0x00000010;
+unsigned int i;
+
+int main(void) {
+    SystemInit();
+    SystemCoreClockUpdate();
+    LPC_PINCON->PINSEL0 = 0x00000000; // Set GPIO
+    LPC_GPIO0->FIODIR = 0x00000FF0;   // Set P0.4 to P0.11 as output (8 LEDs)  
+    while (1) {
+			LED<<=8;
+		  for(i=0;i<9;i++){
+			LPC_GPIO0->FIOSET=LED;
+			LPC_GPIO0->FIOCLR=LED;
+			LED>>=1;}	
+			LED=0X00000010;
+			
+    }
+}
+
+void delay(void) {
+    for (i = 0; i < 1000000; i++);
+}
