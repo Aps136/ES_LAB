8df4bf6ff73efb6388aee8ddc8244948c56e602b
Aps136 <aparnakaranth654@gmail.com>
2025-04-04 11:59:54 +0530

    Update ex1.c

--- lab10/ex1.c
+++ lab10/ex1.c
@@ -1,98 +1,149 @@
-#include <LPC17xx.h>
-#include <stdio.h>
-#include "AN_LCD.h"
-
+#include<LPC17xx.h>
+#include<stdio.h>
+#include<string.h>
 #define Ref_Vtg 3.300
-#define Full_Scale 0xFFF  // 12 bit ADC
+#define Full_Scale 0xFFF // 12 bit ADC
+#define RS_CTRL  0x08000000  // P0.27
+#define EN_CTRL  0x10000000  // P0.28
+#define DT_CTRL  0x07800000  // P0.23 to P0.26 data lines
+unsigned long int init_command[] = {0x30, 0x30, 0x30, 0x20, 0x28, 0x0C, 0x06, 0x01, 0x80};
+unsigned long int temp1 = 0, temp2 = 0, i, j;
+unsigned char flag1 = 0, flag2 = 0;
+
+
+void lcd_write(void);
+void port_write(void);
+void delay_lcd(unsigned int);
 
 int main(void)
 {
-    unsigned long adc_temp_ch4, adc_temp_ch5;
+   unsigned long temp4;
+unsigned long temp5;
+
+    unsigned long adc_temp;
     unsigned int i;
-    float in_vtg_ch4, in_vtg_ch5, diff_vtg;
-    unsigned char vtg_diff[7], dval_diff[7];
-    unsigned char Msg1[11] = {"CH4 - CH5:"};
-    unsigned char Msg2[12] = {"DIFF VALUE:"};
+    float in_vtg;
+    unsigned char vtg[7], dval[7];
+    unsigned char Msg3[] = {"ANALOG IP:"};
+    unsigned char Msg4[] = {"ADC OUTPUT:"};
 
     SystemInit();
     SystemCoreClockUpdate();
-    
-    LPC_SC->PCONP |= (1<<15);  // Power for GPIO block
-    lcd_init();
-    
-    // Configure pins for ADC channels 4 and 5
-    LPC_PINCON->PINSEL3 |= 0xF0000000;  // P1.30 as AD0.4 and P1.31 as AD0.5
-    LPC_SC->PCONP |= (1<<12);  // Enable the peripheral ADC
-    
-    SystemCoreClockUpdate();
-    temp1 = 0x80;  // First line of LCD
-    lcd_com();
-    delay_lcd(800);
-    lcd_puts(&Msg1[0]);
-    
-    temp1 = 0xC0;  // Second line of LCD
-    lcd_com();
-    delay_lcd(800);
-    lcd_puts(&Msg2[0]);
-    
+LPC_GPIO0->FIODIR = DT_CTRL | RS_CTRL | EN_CTRL; // Config output
+    //lcd_init(); // Initialize LCD
+    LPC_PINCON->PINSEL3 |= (3<<30)| (3<<28) ; // P1.31 as AD0.5
+LPC_SC->PCONP |= (1<<12); // Enable the peripheral ADC
+flag1 = 0; // Command
+    for(i = 0; i < 9; i++)
+    {
+        temp1 = init_command[i];
+        lcd_write(); // Send Init commands to LCD
+    }
+
+    flag1 = 1; // Data
+    i = 0;
+    while (Msg3[i++] != '\0')
+    {
+        temp1 = Msg3[i-1];
+        lcd_write(); // Send data bytes
+    }
+
+    flag1 = 0; // Command
+    temp1 = 0xC0; // Cursor at beginning of second line
+    lcd_write();
+    flag1 = 1;
+    i = 0;
+    while (Msg4[i++] != '\0')
+    {
+        temp1 = Msg4[i-1];
+        lcd_write(); // Send data bytes
+    }
+
     while(1)
     {
-        // Read ADC Channel 4
-        LPC_ADC->ADCR = (1<<4)|(1<<21)|(1<<24);  // ADC0.4, start conversion and operational
-        while((adc_temp_ch4 = LPC_ADC->ADGDR) == 0x80000000);  // Wait till conversion complete
-        adc_temp_ch4 = LPC_ADC->ADGDR;
-        adc_temp_ch4 >>= 4;
-        adc_temp_ch4 &= 0x00000FFF;  // 12 bit ADC
-        
-        // Calculate input voltage for channel 4
-        in_vtg_ch4 = (((float)adc_temp_ch4 * (float)Ref_Vtg))/((float)Full_Scale);
-        
-        // Small delay between conversions
-        for(i=0; i<2000; i++);
-        
-        // Read ADC Channel 5
-        LPC_ADC->ADCR = (1<<5)|(1<<21)|(1<<24);  // ADC0.5, start conversion and operational
-        while((adc_temp_ch5 = LPC_ADC->ADGDR) == 0x80000000);  // Wait till conversion complete
-        adc_temp_ch5 = LPC_ADC->ADGDR;
-        adc_temp_ch5 >>= 4;
-        adc_temp_ch5 &= 0x00000FFF;  // 12 bit ADC
-        
-        // Calculate input voltage for channel 5
-        in_vtg_ch5 = (((float)adc_temp_ch5 * (float)Ref_Vtg))/((float)Full_Scale);
-        
-        // Calculate the difference in voltage
-        diff_vtg = in_vtg_ch4 - in_vtg_ch5;
-        
-        // Calculate the digital value difference
-        long diff_digital = (long)adc_temp_ch4 - (long)adc_temp_ch5;
-        
-        // Convert the readings into strings to display on LCD
-        sprintf(vtg_diff, "%+3.2fV", diff_vtg);  // Include sign for voltage difference
-        sprintf(dval_diff, "%+4d", (int)diff_digital);  // Include sign for digital value
-        
-        // Display voltage difference
-        temp1 = 0x8A;  // Position on first line
-        lcd_com();
-        delay_lcd(800);
-        lcd_puts(&vtg_diff[0]);
-        
-        // Display digital value difference
-        temp1 = 0xCB;  // Position on second line
-        lcd_com();
-        delay_lcd(800);
-        lcd_puts(&dval_diff[0]);
-        
-        // Delay before next reading
-        for(i=0; i<200000; i++);
-        
-        // Clear variables for next reading
-        for(i=0; i<7; i++)
-            vtg_diff[i] = dval_diff[i] = 0x00;
-            
-        adc_temp_ch4 = 0;
-        adc_temp_ch5 = 0;
-        in_vtg_ch4 = 0;
-        in_vtg_ch5 = 0;
-        diff_vtg = 0;
+        LPC_ADC->ADCR = (1<<5) | (1<<21) | (1<<24); // ADC0.5, start conversion and operational
+        while(((temp4 = LPC_ADC->ADDR5) & (1<<31)) == 0); // Wait for conversion complete
+for(i=0;i<2000;i++);
+       
+        temp4 = LPC_ADC->ADDR5;
+        temp4 >>= 4;
+        temp4 &= 0x00000FFF; // 12-bit ADC
+
+
+/*LPC_ADC->ADCR = (1<<5)|(1<<21)|(1<<24);// //ADC0.5, start conversion and operational
+for(i=0;i<2000;i++); //delay for conversion
+while(((temp5=LPC_ADC->ADDR5) & (1<<31)) == 0); //wait till 'done' bit is 1, indicates conversion complete
+temp5 = LPC_ADC->ADDR5;
+temp5 >>= 4;
+temp5 &= 0x00000FFF;*/
+adc_temp= temp4;
+
+        in_vtg = (((float)adc_temp * (float)Ref_Vtg) / ((float)Full_Scale)); // Calculating input analog voltage
+        sprintf(vtg, "%3.2fV", in_vtg); // Convert the readings into string to display on LCD
+        sprintf(dval, "%0x", adc_temp);
+
+        flag1 = 0;
+        temp1 = 0x8A;
+        lcd_write();
+        flag1 = 1;
+        i = 0;
+        while (vtg[i++] != '\0')
+        {
+            temp1 = vtg[i-1];
+            lcd_write(); // Send data bytes
+        }
+
+        flag1 = 0;
+        temp1 = 0xCB;
+        lcd_write();
+        flag1 = 1;
+        i = 0;
+        while (dval[i++] != '\0')
+        {
+            temp1 = dval[i-1];
+            lcd_write(); // Send data bytes
+        }
+
+        for(i = 0; i < 7; i++)
+        {
+            vtg[i] = dval[i] = 0;
+        }
     }
+}
+void lcd_write(void)
+{
+    flag2 = (flag1 == 1) ? 0 : ((temp1 == 0x30) || (temp1 == 0x20)) ? 1 : 0; // If command is 0x30 (Working in 8-bit mode initially)
+   
+    temp2 = temp1 & 0xF0;
+    temp2 = temp2 << 19; // Data lines from 23 to 26
+    port_write(); // Output the higher digit on P0.26-P0.23
+   
+    if (!flag2) // Other than command 0x30, send the lower 4-bit also
+    {
+        temp2 = temp1 & 0x0F;
+        temp2 = temp2 << 23;
+        port_write(); // Output the lower digit on P0.26-P0.23
+    }
+}
+
+void port_write(void)
+{
+    LPC_GPIO0->FIOPIN = temp2;
+   
+    if (flag1 == 0)
+        LPC_GPIO0->FIOCLR = RS_CTRL; // Select command register
+    else
+        LPC_GPIO0->FIOSET = RS_CTRL; // Select data register
+   
+    LPC_GPIO0->FIOSET = EN_CTRL; // Apply edge on Enable
+    delay_lcd(25);
+    LPC_GPIO0->FIOCLR = EN_CTRL;
+    delay_lcd(500000);
+}
+
+void delay_lcd(unsigned int r1)
+{
+    unsigned int r;
+    for (r = 0; r < r1; r++);
+    return;
 }
